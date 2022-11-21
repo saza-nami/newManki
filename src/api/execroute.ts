4ad891feeceae6cf4de734274c772c5e9eb90fd2
saza-nami <saza-nami@users.noreply.github.com>
2022-11-21 10:53:36 +0900

    update api
    api の db処理のトランザクション化

--- src/api/execroute.ts
+++ src/api/execroute.ts
@@ -11,16 +11,31 @@ interface ExecRoute extends ApiResult {
   message?: string;
 }
 
-const createOrderSql = "CALL createOrder(?, ?, ?, ?, @a, @b, @c)";
+// lock tables
+const lockTablesSql =
+  "LOCK TABLES orderTable WRITE, userTable WRITE, passableTable READ";
+// unlock tables
+const unlockTables = "UNLOCK TABLES";
+const reqUsersOrderSql =
+  "SELECT orderId FROM userTable \
+  WHERE userId = UUID_TO_BIN(?,1) AND endAt IS NULL \
+  LOCK IN SHARE MODE";
+const insertOrderSql =
+  "INSERT INTO orderTable(route, dest, junkai) VALUES (?, ?, ?)";
+const reqLastOrderIdSql =
+  "SELECT orderId FROM orderTable ORDER BY orderId DESC LIMIT 1";
+const updateUserInfoSql =
+  "UPDATE userTable SET orderId = ? WHERE userId = UUID_TO_BIN(?, 1)";
+const completedOrderSql = "SELECT endAt FROM orderTable WHERE orderId = ?";
 
 async function reserveRoute(
   userId: string,
   route: Position[][],
   junkai: boolean
 ): Promise<ExecRoute> {
+  // return value of API
   const result: ExecRoute = { succeeded: false };
-
-  // 入力チェック
+  // check parameter
   if (
     typeof userId === "undefined" ||
     typeof route === "undefined" ||
@@ -29,13 +44,15 @@ async function reserveRoute(
     return report(result);
   }
 
-  const conn = await db.createNewConn();
+  const conn = await db.createNewConn(); // database connection
+  // begin transaction
   try {
     await conn.beginTransaction();
-    await conn.query("LOCK TABLES orderTable WRITE");
+    await conn.query(lockTablesSql);
+    // check exist user
     if ((await global.existUserTran(conn, userId)) === true) {
-      const passPoints: PassablePoint[] = await map.getAllPassPos(conn);
-      // 経路チェック
+      const passPoints: PassablePoint[] = await map.getPassPos(conn);
+      // check route in passable area
       for (let i = 0; i < route.length; i++) {
         for (let j = 0; j < route[i].length - 1; j++) {
           if (
@@ -47,69 +64,41 @@ async function reserveRoute(
         }
       }
 
+      // insert route in the database
       const dest = global.routeToDest(route);
-      // 命令をDBに登録
       const existOrder = db.extractElem(
-        await db.executeTran(
-          conn,
-          "SELECT orderId FROM userTable \
-          WHERE userId = UUID_TO_BIN(?,1) AND endAt IS NULL \
-          LOCK IN SHARE MODE",
-          [userId]
-        )
+        await db.executeTran(conn, reqUsersOrderSql, [userId])
       );
       if (existOrder !== undefined && "orderId" in existOrder) {
         if (existOrder["orderId"] === null) {
-          await db.executeTran(
-            conn,
-            "INSERT INTO orderTable(route, dest, junkai) VALUES (?, ?, ?)",
-            [route, dest, junkai]
-          );
+          await db.executeTran(conn, insertOrderSql, [route, dest, junkai]);
           const createdOrderId = db.extractElem(
-            await db.executeTran(
-              conn,
-              "SELECT orderId FROM orderTable ORDER BY orderId DESC LIMIT 1"
-            )
+            await db.executeTran(conn, reqLastOrderIdSql)
           );
           if (createdOrderId !== undefined && "orderId" in createdOrderId) {
-            await db.executeTran(
-              conn,
-              "UPDATE userTable SET orderId = ? \
-              WHERE userId = UUID_TO_BIN(?,1) \
-              LOCK IN SHARE MODE",
-              [createdOrderId["orderId"], userId]
-            );
+            await db.executeTran(conn, updateUserInfoSql, [
+              createdOrderId["orderId"],
+              userId,
+            ]);
           }
           result.succeeded = true;
         } else {
           const endAt = db.extractElem(
-            await db.executeTran(
-              conn,
-              "SELECT endAt FROM orderTable WHERE orderId = ?",
-              [existOrder["orderId"]]
-            )
+            await db.executeTran(conn, completedOrderSql, [
+              existOrder["orderId"],
+            ])
           );
           if (endAt !== undefined && "endAt" in endAt) {
             if (endAt["endAt"] !== null) {
-              await db.executeTran(
-                conn,
-                "INSERT INTO orderTable(route, dest, junkai) VALUES (?, ?, ?)",
-                [route, dest, junkai]
-              );
+              await db.executeTran(conn, insertOrderSql, [route, dest, junkai]);
               const createdOrderId = db.extractElem(
-                await db.executeTran(
-                  conn,
-                  "SELECT orderId FROM orderTable ORDER BY orderId DESC LIMIT 1"
-                )
+                await db.executeTran(conn, reqLastOrderIdSql)
               );
               if (createdOrderId !== undefined && "orderId" in createdOrderId) {
-                await db.executeTran(
-                  conn,
-                  "UPDATE userTable SET orderId = ? \
-                  WHERE userId = UUID_TO_BIN(?,1) \
-                  LOCK IN SHARE MODE",
-                  [createdOrderId["orderId"], userId]
-                );
+                await db.executeTran(conn, updateUserInfoSql, [
+                  createdOrderId["orderId"],
+                  userId,
+                ]);
               }
               result.succeeded = true;
             } else {
@@ -124,7 +113,7 @@ async function reserveRoute(
     await conn.rollback();
     console.log(err);
   } finally {
-    await conn.query("UNLOCK TABLES");
+    await conn.query(unlockTables);
     conn.release();
   }
   return report(result);
