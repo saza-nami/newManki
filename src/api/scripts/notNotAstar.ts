3cd399fd6c4f7749f13d7fe3ae49195a07f42f31
Takatomo0424 <96109337+Takatomo0424@users.noreply.github.com>
2023-01-26 13:14:26 +0900

    change manAstar

--- src/api/scripts/notNotAstar.ts
+++ src/api/scripts/notNotAstar.ts
@@ -95,9 +95,89 @@ async function manAstar(
   goal: Position,
   passPoints: PassablePoint[]
 ): Promise<Position[] | null> {
-  const result = Astar(start, goal, passPoints);
-  await new Promise<void>((r) => setTimeout(() => r(), 0));
-  return result;
+  let goalInfo: NodeInfo = {
+    position: goal,
+    gCost: Number.MAX_VALUE,
+    hCost: dirdist.distanceTo(start, goal),
+    comfirm: false,
+    parent: null,
+  };
+  const nodes: NodeInfo[] = [
+    {
+      position: start,
+      gCost: 0,
+      hCost: dirdist.distanceTo(start, goal),
+      comfirm: false,
+      parent: null,
+    },
+  ];
+
+  let foo = 0;
+
+  while (goalInfo.parent === null) {
+    const minIndex = getMinIndex(nodes);
+    if (minIndex < 0) {
+      // 探索失敗
+      return null;
+    } else {
+      // 隣接地点生成
+      nodes[minIndex].comfirm = true;
+      const nextNodes: (NodeInfo | false)[] = map.createNodeInfo(
+        nodes[minIndex],
+        minIndex,
+        goal,
+        passPoints
+      );
+
+      let reachInfo: NodeInfo | false = false;
+      nextNodes.forEach((next) => {
+        if (next != false) {
+          if (map.reachIn(next.position, goal, passPoints)) {
+            const f = next.gCost + dirdist.distanceTo(next.position, goal);
+            if (goalInfo.gCost > f) {
+              reachInfo = next;
+              goalInfo.gCost = f;
+            }
+          }
+        }
+      });
+
+      if (reachInfo) {
+        // 探索終了
+        goalInfo.parent = nodes.length;
+        nodes.push(reachInfo);
+        break;
+      } else {
+        // 同一地点処理
+        nodes.forEach((node, nodeIndex) => {
+          nextNodes.forEach((next, nextIndex) => {
+            if (next != false && map.approx(node.position, next.position)) {
+              if (node.gCost > next.gCost) {
+                next.comfirm = node.comfirm;
+                nodes[nodeIndex] = next;
+              }
+              nextNodes[nextIndex] = false;
+            }
+          });
+        });
+        nextNodes.forEach((next) => {
+          if (next != false) nodes.push(next);
+        });
+      }
+      foo++;
+      if (foo % 7 === 0)
+        await new Promise<void>((r) => setTimeout(() => r(), 0));
+    }
+  }
+
+  let childNode = goalInfo;
+  const result: Position[] = [];
+  while (1) {
+    result.unshift(childNode.position);
+    if (childNode.parent === null) break;
+    childNode = nodes[childNode.parent];
+  }
+  return optimization(result, passPoints);
 }
 
 // 評価済みノード群から最小コストノードの算出
